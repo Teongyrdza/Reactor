c6adbce15c17d6b364c63fb00d98927eb562e2ca
Ostap <ostap.hnatenko@gmail.com>
2021-06-16 10:16:29 +0300

    Initial Commit

---
+++ Tests/ReactorTests/ReactorTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import Reactor
+
+    final class ReactorTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(Reactor().text, "Hello, World!")
+        }
+    }
